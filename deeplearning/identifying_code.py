e7f96815f9e121d06a5e4ff38f6cbf53fb7e7697
a1790927907 <1790927907@qq.com>
2020-09-30 21:43:07 +0800

    small project

---
+++ deeplearning/identifying_code.py
@@ -0,0 +1,188 @@
+'''
+数据集：https://wws.lanzous.com/ijlcmh29pmf
+电脑原因，仅训练100个数据，数据过多内存溢出了。
+需要迭代420次以上
+'''
+
+import string
+import tensorflow as tf
+import pandas as pd
+import os
+
+#数据预处理，将数据中的验证码的四个字母转换成在所有数字，小写字母，大写字母中的索引
+def data_proceeding(data):
+    upper_letter = string.ascii_uppercase
+
+    all_list = list(upper_letter)
+    # all_dict = {i: j for i, j in enumerate(all_list)}
+    data_str = data[1]
+    index_str_list = []
+    for i in data_str.tolist():
+        sp = []
+        for j in i:
+            sp.append(all_list.index(j))
+        index_str_list.append(sp)
+    return (index_str_list,all_list)
+
+#获取文件名队列
+def get_file_queue(path,num,train=True):
+    file_name = os.listdir(path)
+    remove_list = []
+    for i in file_name:
+        #去除不需要的文件
+        try:
+            int(i.split('.')[0])
+        except:
+            remove_list.append(i)
+    for j in remove_list:
+        file_name.remove(j)
+
+    #排序，确保读取后的特征值和目标值一一对应
+    file_name.sort(key=lambda x:int(x.split('.')[0]))
+    #为训练集则直接从第一个文件开始取
+    if train:
+        file_queue = [os.path.join(path,name) for name in file_name if int(name.split('.')[0]) <= (num-1)]
+    #为测试集则从训练集之后的一个文件取
+    else:
+        file_queue = [os.path.join(path, name) for name in file_name if (train_num <= int(name.split('.')[0]) <= (num + train_num - 1))]
+    return file_queue
+
+#图片卷积(两层卷积)
+#传入图片格式遵从[batch,height,weight,channel]
+def convolute(image):
+    with tf.variable_scope('convolute'):
+        #第一次卷积
+        #卷积核使用32个,3*3
+        fil1 = tf.Variable(initial_value=tf.random_normal(shape=[3,3,3,32]))
+        fil1_bias = tf.Variable(initial_value=tf.random_normal(shape=[32]))
+        feature1 = tf.nn.conv2d(image,fil1,[1,1,1,1],'SAME') + fil1_bias
+        #relu激活函数
+        relu_feature1 = tf.nn.relu(feature1)
+        #池化
+        max_pool_feature1 = tf.nn.max_pool(relu_feature1,[1,2,2,1],[1,2,2,1],'SAME')
+
+        #第二次卷积
+        #卷积核使用64个，3*3
+        fil2 = tf.Variable(initial_value=tf.random_normal(shape=[3,3,32,64]))
+        fil2_bias = tf.Variable(initial_value=tf.random_normal(shape=[64]))
+        feature2 = tf.nn.conv2d(max_pool_feature1,fil2,[1,1,1,1],'SAME') + fil2_bias
+        #relu激活函数
+        relu_feature2 = tf.nn.relu(feature2)
+        #池化
+        #最终图像变为[train_num,20,20,64]
+        max_pool_feature2 = tf.nn.max_pool(relu_feature2,[1,2,2,1],[1,2,2,1],'SAME')
+
+    with tf.variable_scope('create_w_b_full_connection'):
+        # 将最终结果转换成二维的作为特征值
+        x = tf.reshape(max_pool_feature2, [train_num, 5 * 20 * 64])
+        #定义权重系数
+        w = tf.Variable(initial_value=tf.random_normal(shape=[5*20*64,all_labels_num*4]))
+        b = tf.Variable(initial_value=tf.random_normal(shape=[all_labels_num*4]))
+        # 全连接
+        y_predict = tf.matmul(x,w) + b
+
+    return (y_predict,fil1,fil2,fil1_bias,fil2_bias,w,b)
+
+#测试集图片在最优卷积核下卷积
+def convolute_test(image_test,fil1,fil2,fil1_bias,fil2_bias):
+    feature1 = tf.nn.conv2d(image_test,fil1,[1,1,1,1],'SAME') + fil1_bias
+    #relu激活函数
+    relu_feature1 = tf.nn.relu(feature1)
+    #池化
+    max_pool_feature1 = tf.nn.max_pool(relu_feature1,[1,2,2,1],[1,2,2,1],'SAME')
+
+    feature2 = tf.nn.conv2d(max_pool_feature1,fil2,[1,1,1,1],'SAME') + fil2_bias
+    #relu激活函数
+    relu_feature2 = tf.nn.relu(feature2)
+    #池化
+    #最终图像变为[train_num,20,20,64]
+    max_pool_feature2 = tf.nn.max_pool(relu_feature2,[1,2,2,1],[1,2,2,1],'SAME')
+
+    #将最终结果转换成二维的作为特征值
+    x = tf.reshape(max_pool_feature2,[test_num,5*20*64])
+    return x
+
+
+#定义训练集数据个数
+tf.flags.DEFINE_integer('train_num',100,'训练集数据个数')
+#定义测试集数据个数
+tf.flags.DEFINE_integer('test_num',100,'测试集数据个数')
+
+FLAGS = tf.flags.FLAGS
+train_num = FLAGS.train_num
+test_num = FLAGS.test_num
+
+
+#目标值预处理(全部改为one-hot编码表示)
+data = pd.read_csv('./image/02-代码/GenPics/labels.csv',header=None,iterator=True).get_chunk(train_num)
+new_column,all_list = data_proceeding(data)
+
+#所有类别的个数
+all_labels_num = len(all_list)
+
+#将目标值进行one-hot编码
+tf_target = tf.constant(new_column)
+one_hot_target = tf.one_hot(tf_target,all_labels_num)
+one_hot_target = tf.cast(one_hot_target,tf.float32)
+
+#文件读取+批处理
+file_name_list = get_file_queue('./image/02-代码/GenPics',train_num)
+#取消shuffle打乱队列顺序，确保特征值与目标值一一对应
+file_queue = tf.train.string_input_producer(file_name_list,shuffle=False)
+reader = tf.WholeFileReader()
+key,value = reader.read(file_queue)
+img_decoded = tf.image.decode_jpeg(value)
+img_resized = tf.image.resize_images(img_decoded,(80,20))
+img_reshaped = tf.reshape(img_resized,shape=(20,80,3))
+img_batch = tf.train.batch([img_reshaped],train_num,2,train_num)
+img_batch = tf.cast(img_batch,tf.float32)
+
+
+#先将one-hot编码的目标值变为二维的
+#三维保留，之后验证正确率时需要使用
+one_hot_target_two_dim = tf.reshape(one_hot_target,shape=[train_num,-1])
+
+#获取预测值
+#最终返回的y_predict的形状为[train_num,62*4]
+y_predict,fil1,fil2,fil1_bias,fil2_bias,w,b = convolute(img_batch)
+
+#验证正确率，只有四个全为True时才返回True
+#用reduce_all
+with tf.variable_scope('calc_accruacy'):
+    y_predict_used_to_calc_accuarcy = tf.reshape(y_predict,shape=[train_num,4,all_labels_num])
+    y_predict_max_loc = tf.argmax(y_predict_used_to_calc_accuarcy,axis=-1)
+    target_max_loc = tf.argmax(one_hot_target,axis=-1)
+    boll_judge = tf.equal(y_predict_max_loc,target_max_loc)
+    all_judge = tf.reduce_all(boll_judge,axis=1)
+    accuracy = tf.reduce_mean(tf.cast(all_judge,tf.float32))
+
+#使用sigmoid+交叉熵来计算损失
+#从公式上看，softmax适合一个样本对应一个目标值，而sigmoid适合一个样本对应多个目标值
+with tf.variable_scope('calc_loss'):
+    loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=one_hot_target_two_dim,logits=y_predict)
+    loss = tf.reduce_mean(loss)
+
+#创建亚当优化器
+with tf.variable_scope('create_optimizer'):
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+    optimizer_min = optimizer.minimize(loss)
+
+saver = tf.train.Saver()
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess,coord)
+    sess.run(init)
+    for i in range(1000):
+        sess.run(optimizer_min)
+        print(sess.run(loss),end='')
+        print(f'准确率为{accuracy.eval()}')
+        # saver.save(sess,'captcha.ckpt')
+    # saver.restore(sess,'captcha.ckpt')
+    coord.request_stop()
+    coord.join(threads)
+
+
+
+
+
---
+++ deeplearning/minist.py
@@ -0,0 +1,213 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import os
+os.environ['TP_CPP_MIN_LOG_LEVEL'] = '2'
+
+#n为卷积次数
+#x为需要进行卷积的样本(传入shape为[batch,height,width,channels]，可为None)
+#filter_num为基础卷积次数
+#F为卷积核的shape，填入长或宽其中一个即可，一般都是1或3或5
+#multi参数为每次迭代卷积核的个数乘数的基数
+#y_num参数为目标值类别的个数
+#此函数不适用
+def CNN_y_predict(x,n=2,filter_num=32,F=3,multi=2,y_num=10):
+    #定义卷积核权重系数,需要梯度下降训练，用变量表示
+    #由于多次卷积，这里用字典表示
+    w_filters = {}
+    b_filter = {}
+    features = {}
+    relu_features = {}
+    max_pool_features = {}
+    x_copy = x
+    base_channel = list(x_copy.shape)
+    for i in range(len(base_channel)):
+        try:
+            base_channel[i] = int(base_channel[i])
+        except:
+            pass
+
+    for i in range(n):
+        b_filter[i] = tf.Variable(initial_value=tf.random_normal(shape=[filter_num], dtype=tf.float32))
+        w_filters[i] = tf.Variable(initial_value=tf.random_normal(shape=[F,F,base_channel[-1],filter_num],dtype=tf.float32)) + b_filter[i]
+        #卷积
+        features[i] = tf.nn.conv2d(x_copy,w_filters[i],[1,1,1,1],'SAME')
+        #relu激活函数
+        relu_features[i] = tf.nn.relu(features[i])
+        #最大值池化
+        max_pool_features[i] = tf.nn.max_pool(relu_features[i],[1,2,2,1],[1,2,2,1],'SAME')
+
+        #计算池化后出来的shape,更新shape信息
+        if base_channel[1] % 2 == 0:
+            base_channel = (base_channel[0],int(base_channel[1] / 2),int(base_channel[2] / 2),filter_num)
+        else:
+            base_channel = (base_channel[0],int(base_channel[1] / 2)+1,int(base_channel[2] / 2)+1,filter_num)
+        filter_num = filter_num * multi
+        x_copy = max_pool_features[i]
+
+    if str(base_channel[0]) == '?':
+        base_channel = list(base_channel)
+        base_channel[0] = -1
+
+    x_copy = tf.reshape(x_copy,(base_channel[0],base_channel[1]*base_channel[2]*base_channel[3]))
+    #计算出最后的w权重系数
+    w = tf.Variable(initial_value=tf.random_normal(shape=(base_channel[1]*base_channel[2]*base_channel[3],y_num)))
+    b = tf.Variable(initial_value=tf.random_normal(shape=[y_num]))
+    y_predict = tf.matmul(x_copy,w) + b
+    return (y_predict,w_filters,b_filter,features,relu_features,max_pool_features)
+
+#卷积操作函数，传入图片进行两次卷积 - 激活 - 池化
+def CNN_num_two(images_):
+    with tf.variable_scope('create_convolution_ratio'):
+        # 第一次大卷积层
+        # 创建3*3卷积核,32个卷积核
+        fil1 = tf.Variable(initial_value=tf.random_normal(shape=[3, 3, 1, 32]))
+        # 卷积操作
+        # 卷积结束后输出形状为[-1,28,28,32]
+        # 卷积需要加上偏置
+        features1 = tf.nn.conv2d(images_, fil1, [1, 1, 1, 1], 'SAME') + tf.Variable(initial_value=tf.random_normal(shape=[32]))
+        # Relu激活函数
+        # 激活函数不变shape
+        relu_features1 = tf.nn.relu(features1)
+        # 池化
+        # 池化之后输出形状为[-1,14,14,32]
+        max_pool_features1 = tf.nn.max_pool(relu_features1, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME')
+        # print(max_pool_features1)
+        # 第二次大卷积层
+        # 使用第一次池化完毕的输出作为输入
+        # 增加到64个卷积核
+        fil2 = tf.Variable(initial_value=tf.random_normal(shape=[3, 3, 32, 64]))
+        # 输出结束后shape为[-1,14,14,64]
+        features2 = tf.nn.conv2d(max_pool_features1, fil2, [1, 1, 1, 1], 'SAME') + tf.Variable(initial_value=tf.random_normal(shape=[64]))
+
+        # Relu第二次激活
+        relu_features2 = tf.nn.relu(features2)
+
+        # 第二次池化
+        # 最终输出为[-1,7,7,64]
+        max_pool_features2 = tf.nn.max_pool(relu_features2, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME')
+
+        # 将最后输出形状改变成符合softmax函数的形状，一般是二维
+        # 最后输出形状即为通过神经网络隐藏层后的特征值
+        x = tf.reshape(max_pool_features2, [-1, 7 * 7 * 64])
+
+        return x,fil1,fil2
+
+#输出测试集卷积-激活-池化后的输出
+def CNN_test_img(img_test_CNN,fil1,fil2):
+    # 卷积操作
+    # 卷积结束后输出形状为[-1,28,28,32]
+    features1 = tf.nn.conv2d(img_test_CNN, fil1, [1, 1, 1, 1], 'SAME')
+    # Relu激活函数
+    # 激活函数不变shape
+    relu_features1 = tf.nn.relu(features1)
+    # 池化
+    # 池化之后输出形状为[-1,14,14,32]
+    max_pool_features1 = tf.nn.max_pool(relu_features1, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME')
+
+    # 输出结束后shape为[-1,14,14,64]
+    features2 = tf.nn.conv2d(max_pool_features1, fil2, [1, 1, 1, 1], 'SAME')
+
+    # Relu第二次激活
+    relu_features2 = tf.nn.relu(features2)
+
+    # 第二次池化
+    # 最终输出为[-1,7,7,64]
+    max_pool_features2 = tf.nn.max_pool(relu_features2, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME')
+
+    # 将最后输出形状改变成符合softmax函数的形状，一般是二维
+    # 最后输出形状即为通过神经网络隐藏层后的特征值
+    x = tf.reshape(max_pool_features2, [-1, 7 * 7 * 64])
+
+    return x
+
+
+
+minist_data = input_data.read_data_sets('02-代码/mnist_data',one_hot=True)
+
+images = tf.placeholder(dtype=tf.float32,shape=(None,784))
+labels = tf.placeholder(dtype=tf.float32,shape=(None,10))
+images_ = tf.reshape(images,[-1,28,28,1])
+
+x,fil1,fil2 = CNN_num_two(images_)
+
+with tf.variable_scope('create_w_b'):
+    # 构建w系数
+    w = tf.Variable(initial_value=tf.random_normal(shape=[7 * 7 * 64, 10]))
+    b = tf.Variable(initial_value=tf.random_normal(shape=[10]))
+    y_predict = tf.matmul(x, w) + b
+
+#构建测试集
+img_test = tf.placeholder(tf.float32,shape=(None,784))
+label_test = tf.placeholder(tf.float32,shape=(None,10))
+img_test_CNN = tf.reshape(img_test,[-1,28,28,1])
+img_test_ = CNN_test_img(img_test_CNN,fil1,fil2)
+
+#计算测试集结果
+y_test = tf.matmul(img_test_,w) + b
+
+#求出测试集结果中的最大值所在位置即为对应类别
+#横向求，求每一排的最大值，因为上面是x*w，所以特征是横着的
+test_loc = tf.argmax(y_test,axis=1)
+
+#求出真实值的对应类别位置
+true_loc = tf.argmax(label_test,axis=1)
+
+#比对两者是否相同，相同返回True，不同返回False
+bool_j = tf.equal(true_loc,test_loc)
+
+#最后需要求出准确率，bool型无法求，所以需要转换类型
+accuracy = tf.reduce_mean(tf.cast(bool_j,tf.float32))
+
+
+# y_predict,w_filters,b_filter,features,relu_features,max_pool_features = CNN_y_predict(images_)
+with tf.variable_scope('calc_loss'):
+    #构建全连接层，计算损失
+    loss = tf.nn.softmax_cross_entropy_with_logits(labels=labels,logits=y_predict)
+    loss = tf.reduce_mean(loss)
+
+with tf.variable_scope('create_optimizer'):
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+    optimizer_min = optimizer.minimize(loss)
+
+
+tf.summary.scalar('loss',loss)
+tf.summary.histogram('w',w)
+tf.summary.histogram('b',b)
+tf.summary.histogram('filter_1',fil1)
+tf.summary.histogram('filter_2',fil2)
+merged = tf.summary.merge_all()
+
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    sess.run(init)
+    images_all,labels_all = minist_data.train.next_batch(300)
+    img_test_all,label_test_all = minist_data.test.next_batch(100)
+    # file_writer = tf.summary.FileWriter('./temp/',graph=sess.graph)
+    # for i in range(500):
+    #     sess.run(optimizer_min,feed_dict={labels:labels_all,images:images_all})
+    #     print(sess.run(loss,feed_dict={labels:labels_all,images:images_all}))
+    #
+    #     summary = sess.run(merged,feed_dict={labels:labels_all,images:images_all})
+    #     file_writer.add_summary(summary)
+    #     saver.save(sess,'./temp/minist.ckpt')
+
+    saver.restore(sess,'./temp/minist.ckpt')
+    print(sess.run(accuracy,feed_dict={img_test:img_test_all,label_test:label_test_all}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
